e60e62cc43eb0f57aff7b3a28fa6b233dd23e6c6
Nacho <nacho.pena.navarrete@gmail.com>
2018-11-16 14:00:05 +0100

    Animación navbar

---
+++ Proyectos/Proyecto3/assets/js/menutransition.js
@@ -0,0 +1,10 @@
+(function(){
+    let element = document.querySelector('header nav');
+    document.addEventListener('scroll', event => {
+        if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            element.classList.add('shape-nav');
+        }else {
+            element.classList.remove('shape-nav');
+        }
+    });
+})();
